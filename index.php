7391703d1bad096a3040500005f3a36c2f098ccf
manikj88 <129405143+manikj88@users.noreply.github.com>
2023-04-05 18:34:19 +0530

    index added

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>SP</title>
+</head>
+<body>
+<?php include("header.php"); ?>
+
+<br />
+<br />
+<b><a href="">Masters</a></b>
+<br />
+<br />
+<b><a href="">Purchase</a></b>
+<br />
+<br />
+<b><a href="">Exp</a></b>
+<br />
+<br />
+<b><a href="">Prod</a></b>
+<br />
+job card, design, designation, chemicals, challan no.
+<br />
+<br />
+<b><a href="">Sale</a></b>
+<br />
+<br />
+	
+</body>
+</html>
